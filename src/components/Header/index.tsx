0aed8cfe27b578ddba76a2582748999f6c82aa12
Bruno Alves <vespidhook@gmail.com>
2023-01-05 19:04:35 -0300

    transactions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,15 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
 export function Header() {
   return (
-    <header>
-      Header
-    </header>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dtmoney" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content>
+    </Container>
   );
 }
