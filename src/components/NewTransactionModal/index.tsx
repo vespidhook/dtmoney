6d83f209bd0c0f92725275e95788f3a946243a45
Bruno Alves <vespidhook@gmail.com>
2023-01-08 16:57:03 -0300

    iniciando contexto

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container, TransactionTypeCotainer } from './styles';
-import { useState } from 'react';
+import { Container, TransactionTypeCotainer, RadioBox } from './styles';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,7 +12,23 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactinModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type,
+    };
+
+    api.post('/transactions', data);
+  }
 
   return (
     <Modal 
@@ -25,38 +42,48 @@ export function NewTransactinModal({ isOpen, onRequestClose }: NewTransactionMod
           <img src={closeImg} alt="Fechar modal" />
         </button>
 
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar transação</h2>
 
           <input
             placeholder="Título"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
           />
 
           <input
             type="number"
             placeholder="Valor"
+            value={value}
+            onChange={event => setValue(Number(event.target.value))}
           />
 
           <TransactionTypeCotainer>
-            <button 
+            <RadioBox 
               type="button"
               onClick={() => { setType('deposit') }}
+              isActive={type === 'deposit'}
+              activeColor="green"
             >
               <img src={incomeImg} alt="Entrada" />
               <span>Entrada</span>
-            </button>
+            </RadioBox>
 
-            <button 
+            <RadioBox 
               type="button"
               onClick={() => { setType('withdraw') }}
+              isActive={type === 'withdraw'}
+              activeColor="red"
             >
               <img src={outcomeImg} alt="Saida" />
               <span>Saida</span>
-            </button>
+            </RadioBox>
           </TransactionTypeCotainer>
 
           <input
             placeholder="Categoria"
+            value={category}
+            onChange={event => setCategory(event.target.value)}
           />
 
           <button type="submit">
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -60,34 +60,47 @@ export const TransactionTypeCotainer = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   gap: 0.5rem;
+`;
 
-  button {
-    height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
 
-    background: transparent;
+const colors = {
+  green: "#33cc95",
+  red: "#e52e4d",
+}
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
-    
-    &:hover {
-      border-color: ${darken(0.1, '#d7d7d7')};
-    }
+  background: ${(props) => (props.isActive 
+    ? transparentize(0.9, colors[props.activeColor])
+    : "transparent")
+  };
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  transition: border-color 0.2s;
+  
+  &:hover {
+    border-color: ${darken(0.1, '#d7d7d7')};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
   }
 `;
